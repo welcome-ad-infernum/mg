febec08bf05594bc9917980f065dab63001e1072
VladStarr <vladstarrr@icloud.com>
2022-03-03 17:48:40 +0200

    Add standalone linux install script

--- examples/systemd/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wget -q https://github.com/welcome-ad-infernum/mg/releases/latest/download/mg_linux_amd64 -O /usr/local/bin/mg
-chmod +x /usr/local/bin/mg
-cp -r mg.service /etc/systemd/system/mg.service
-systemctl daemon-reload
-systemctl start mg.service
-systemctl status mg.service
--- examples/systemd/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-systemctl stop mg.service
-rm -rf /etc/systemd/system/mg.service
-systemctl daemon-reload
-rm -rf /usr/local/bin/mg
