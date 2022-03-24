186295d41a03ac37f6df478b6aa1590d4665a59a
VladStarr <vladstarrr@icloud.com>
2022-03-24 21:43:09 +0200

    Fix azure container group name output

--- examples/terraform/azure/outputs.tf
+++ examples/terraform/azure/outputs.tf
@@ -5,5 +5,5 @@ output "azurerm_resource_group_name" {
 
 output "azurerm_container_group_name" {
   description = "Azure container group name"
-  value       = azurerm_container_group.mg.name
+  value       = "${azurerm_container_group.mg.*.name}"
 }
